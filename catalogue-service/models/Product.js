a8de9c822b7726fc3b9df572d31b6668f20ae886
johnnyhelloworld1 <johnny58723826@hotmail.fr>
2025-04-18 14:19:53 +0200

    implements microservice catalogue

---
+++ catalogue-service/models/Product.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    images: {
+        type: [String],
+        default: []
+    }
+}, {
+    timestamps: true
+});
+
+const Product = mongoose.model('Product', ProductSchema);
+
+module.exports = Product;
